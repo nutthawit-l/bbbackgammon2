e086aec3d53f8b63744b3d88ad4d1228ab00ca18
Nutthawit Lilawannasin <nutthawit.ll@proton.me>
2026-06-07 09:12:40 +0700

    feat: route / to Home and /game to the board

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import { App } from './App';
+import '@fontsource/cinzel/700.css';
+import '@fontsource/cinzel/900.css';
+import '@fontsource/inter/500.css';
+import '@fontsource/inter/700.css';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 );
